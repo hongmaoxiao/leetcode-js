d3f3aaedd69c3a3f175594dfeb625c090f9db662
hongmaoxiao <769501294@qq.com>
2023-02-11 06:28:15 +0800

    add NO.83 another solution

--- 83.js
+++ 83.js
@@ -26,3 +26,16 @@ const deleteDuplicates = (head) => {
 
   return head
 };
+
+const deleteDuplicates1 = (head) => {
+  let retHead = head
+
+  while (head && head.next) {
+    while (head && head.next && head.val === head.next.val) {
+      head.next = head.next.next
+    }
+    head = head.next
+  }
+
+  return retHead
+};
