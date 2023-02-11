ddcadd4dccce1534b8be3b690b3fad2a6726ff0f
hongmaoxiao <769501294@qq.com>
2023-02-11 13:20:44 +0800

    add NO.876 another solution

--- 876.js
+++ 876.js
@@ -9,6 +9,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// 快慢指针法
 const middleNode = (head) => {
   let fast = head
   let slow = head
@@ -23,4 +24,36 @@ const middleNode = (head) => {
   }
 
   return slow
+};
+
+// 数组法
+const middleNode1 = (head) => {
+  let arr = [head]
+
+  while (arr[arr.length - 1].next !== null) {
+    arr.push(arr[arr.length - 1].next)
+  }
+
+  return arr[Math.trunc(arr.length / 2)]
+};
+
+// 单指针法
+const middleNode2 = (head) => {
+  let n = 0
+  let curr = head
+
+  while (curr !== null) {
+    n++
+    curr = curr.next
+  }
+
+  let k = 0
+  curr = head
+
+  while (k < Math.trunc(n / 2)) {
+    k++
+    curr = curr.next
+  }
+
+  return curr
 };
