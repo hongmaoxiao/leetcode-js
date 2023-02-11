7a31bda1480be67b26ca762fad22f2e5926dc103
hongmaoxiao <769501294@qq.com>
2023-02-12 00:31:40 +0800

    add NO.92 another solution

--- 92.js
+++ 92.js
@@ -45,3 +45,54 @@ const reverseBetween = (head, left, right) => {
 
   return curr
 };
+
+// 翻转链表
+const reverseNode = (head) => {
+  let prev = null, curr = head
+
+  while (curr) {
+    const temp = curr.next
+    curr.next = prev
+    prev = curr
+    curr = temp
+  }
+
+  return head
+}
+
+const reverseBetween1 = (head, left, right) => {
+  if (!head || !head.next || left === right) {
+    return head
+  }
+
+  const dummyNode = new ListNode(-1)
+  dummyNode.next = head
+
+  // 被截断左边节点的前一个node
+  let pre = dummyNode
+
+  for (let i = 0; i < left - 1; i++) {
+    pre = pre.next
+  }
+
+  // 截断链表的最右一个node
+  let rightNode = pre
+  for (let j = 0; j < right - left + 1; j++) {
+    rightNode = rightNode.next
+  }
+
+  // 截断链表
+  let leftNode = pre.next
+  // 被截断后右半边剩余的链表
+  let rightNext = rightNode.next
+
+  pre.next = null
+  rightNode.next = null
+
+  reverseNode(leftNode)
+
+  pre.next = rightNode
+  leftNode.next = rightNext
+ 
+  return dummyNode.next
+};
