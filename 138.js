386fbe5e98de987b45f69eec17a9c1eb8999ca66
hongmaoxiao <769501294@qq.com>
2023-02-14 20:13:44 +0800

    add NO.138 another solution

--- 138.js
+++ 138.js
@@ -23,3 +23,28 @@ const copyRandomList = (head, cachedNode = new Map()) => {
 
     return cachedNode.get(head)
 };
+
+const copyRandomList1 = (head) => {
+  if (!head) {
+    return head
+  }
+
+  for (let node = head; node !== null; node = node.next.next) {
+    const nodeNew = new Node(node.val, node.next, null)
+    node.next = nodeNew
+  }
+
+  for (let node = head; node !== null; node = node.next.next) {
+    const nodeNew = node.next
+    nodeNew.random = node.random ? node.random.next : null
+  }
+
+  const newHead = head.next
+  for (let node = head; node !== null; node = node.next) {
+    const nodeNew = node.next
+    node.next = node.next.next
+    nodeNew.next = nodeNew.next ? nodeNew.next.next : null
+  }
+
+  return newHead
+};
