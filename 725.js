22c3a4e2dd8e049c83bd337deb4be046cd910c14
hongmaoxiao <769501294@qq.com>
2023-02-15 13:50:16 +0800

    add NO.725  Split Linked List in Parts

---
+++ 725.js
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode[]}
+ */
+const splitListToParts = (head, k) => {
+  if (k === 1) {
+    return [head]
+  }
+
+  let len = 0
+  let curr = head
+  while (curr) {
+    len++
+    curr = curr.next
+  }
+
+  const div = Math.floor(len / k)
+  let remain = k > len ? 0 : len % k
+
+  let dummyNode = new ListNode(-1, head)
+
+  let arr = []
+  curr = head
+  for (let i = 0; i < k; i++) {
+    if (!dummyNode.next) {
+      arr.push(null)
+      continue
+    }
+    let j = 0
+    while (j < div - 1) {
+      if (curr) {
+        curr = curr.next
+      }
+      j++
+    }
+    if (remain > 0) {
+      if (curr) {
+        curr = curr.next
+      }
+      remain--
+    }
+    if (curr) {
+      const temp = curr.next
+      curr.next = null
+      curr = temp
+      arr.push(dummyNode.next)
+      dummyNode.next = curr
+    } else {
+      arr.push(dummyNode.next)
+      dummyNode.next = null
+    }
+  }
+
+  return arr
+};
