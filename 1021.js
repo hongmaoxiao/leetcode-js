5e692507c8fb1e60377607c3ab6727fea958d23c
hongmaoxiao <769501294@qq.com>
2023-02-16 22:46:33 +0800

    add NO.1021 another solution

--- 1021.js
+++ 1021.js
@@ -21,3 +21,23 @@ const removeOuterParentheses = (s) => {
 
   return res
 };
+
+const removeOuterParentheses1 = (s) => {
+  let level = 0
+  let res = ''
+
+  for (let i = 0; i < s.length; i++) {
+    const curr = s.charAt(i)
+    if (curr === ')') {
+      level--
+    }
+    if (level > 0) {
+      res = `${res}${curr}`
+    }
+    if (curr === '(') {
+      level++
+    }
+  }
+
+  return res
+};
