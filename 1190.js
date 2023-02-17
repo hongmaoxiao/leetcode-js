f4cd9ff181299f30a8a1a04b7141c6b9b2c8f0c0
hongmaoxiao <769501294@qq.com>
2023-02-17 11:11:34 +0800

    add NO.1190 another solution

--- 1190.js
+++ 1190.js
@@ -21,3 +21,33 @@ const reverseParentheses = (s) => {
 
   return str
 };
+
+const reverseParentheses1 = (s) => {
+  const len = s.length
+  const pair = new Array(len).fill(0)
+  const stack = []
+  
+  for (let i = 0; i < len; i++) {
+    if (s[i] === '(') {
+      stack.push(i)
+    } else if (s[i] === ')') {
+      const pop = stack.pop()
+      pair[i] = pop
+      pair[pop] = i
+    }
+  }
+
+  const sb = []
+  let index = 0, step = 1
+  while (index < len) {
+    if (s[index] === '(' || s[index] === ')') {
+      index = pair[index]
+      step = -step
+    } else {
+      sb.push(s[index])
+    }
+    index += step
+  }
+
+  return sb.join('')
+};
