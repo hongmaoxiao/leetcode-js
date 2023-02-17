2050021e6c085e03a5d4b84427a3d13142128497
hongmaoxiao <769501294@qq.com>
2023-02-17 21:30:18 +0800

    add NO.227 another solution

--- 227.js
+++ 227.js
@@ -87,3 +87,46 @@ const isNumber = (val) => {
   const parse = parseInt(val)
   return Number.isInteger(parse)
 }
+
+const calculate1 = (s) => {
+  s = s.trim()
+  const stack = []
+  let preSign = '+'
+  let num = 0
+  const n = s.length
+
+  for (let i = 0; i < n; i++) {
+    if (!isNaN(Number(s[i])) && s[i] !== ' ') {
+      num = num * 10 + s[i].charCodeAt() - '0'.charCodeAt()
+    }
+    if (isNaN(Number(s[i])) || i === n - 1) {
+      switch (preSign) {
+        case '+':
+          stack.push(num)
+          break;
+
+        case '-':
+          stack.push(-num)
+          break;
+
+        case '*':
+          stack.push(stack.pop() * num)
+          break;
+
+        default:
+          stack.push(stack.pop() / num | 0)        
+      }
+
+      preSign = s[i]
+      num = 0
+    }
+  }
+
+  let sum = 0
+  for (let i = 0; i < stack.length; i++) {
+    const curr = stack[i];
+    sum += curr
+  }
+
+  return sum
+}
