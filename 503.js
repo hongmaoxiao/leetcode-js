792cd18aba83ddea0c0be0781e5f9a7990484c72
hongmaoxiao <769501294@qq.com>
2023-02-17 23:02:53 +0800

    add NO.503 another solution

--- 503.js
+++ 503.js
@@ -29,6 +29,23 @@ const nextGreaterElements = (nums) => {
 
     i++
   }
-  
+
   return stack
 };
+
+// 单调栈
+const nextGreaterElements1 = (nums) => {
+  const n = nums.length
+  const ret = new Array(n).fill(-1)
+  const stk = []
+
+  for (let i = 0; i < 2 * n - 1; i++) {
+    while (stk.length && nums[stk[stk.length - 1]] < nums[i % n]) {
+      ret[stk[stk.length - 1]] = nums[i % n]
+      stk.pop()
+    }
+    stk.push(i % n)
+  }
+
+  return ret
+}
