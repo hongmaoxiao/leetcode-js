404d4f010f3a5abcd39ef13a65b11ccab145a3de
hongmaoxiao <769501294@qq.com>
2023-02-18 11:08:34 +0800

    add NO.856 another solution

--- 856.js
+++ 856.js
@@ -35,3 +35,41 @@ const scoreOfParentheses = (s) => {
 
   return sum
 };
+
+const scoreOfParentheses1 = (s) => {
+  const len = s.length
+  let res = 0
+  let dep = 0
+  for (let i = 0; i < len; i++) {
+    const curr = s[i];
+    dep += curr === '(' ? 1 : -1
+    if (curr === ')' && s[i - 1] === '(') {
+      res += Math.pow(2, dep)
+      dep -= 1
+    }
+  }
+
+  return res
+}
+
+const scoreOfParentheses2 = (s) => {
+  if (s.length === 2) {
+    return 1
+  }
+
+  let bal = 0, n = s.length, len = 0
+  for (let i = 0; i < n; i++) {
+    const curr = s[i];
+    bal += curr === '(' ? 1 : -1
+    if (bal === 0) {
+      len = i + 1
+      break
+    }
+  }
+
+  if (len === n) {
+    return 2 * scoreOfParentheses2(s.slice(1, len - 1))
+  } else {
+    return scoreOfParentheses2(s.slice(0, len)) + scoreOfParentheses2(s.slice(len))
+  }
+}
