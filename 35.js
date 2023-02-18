d65b420080af9b06eadf460e22f25cc8acce641c
hongmaoxiao <769501294@qq.com>
2023-02-18 23:52:11 +0800

    add NO.35 another solution

--- 35.js
+++ 35.js
@@ -20,3 +20,19 @@ const searchInsert = (nums, target) => {
 
   return res + 1
 };
+
+const searchInsert1 = (nums, target) => {
+  let low = 0, high = nums.length - 1, res = nums.length
+  
+  while (low <= high) {
+    const mid = (low + high) >> 1
+    if (nums[mid] >= target) {
+      res = mid
+      high = mid - 1
+    } else {
+      low = mid + 1
+    }
+  }
+
+  return res
+};
