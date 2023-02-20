a6e171355be1d403419f3aef823d4e4212de702a
hongmaoxiao <769501294@qq.com>
2023-02-20 14:26:45 +0800

    add NO.409 optimize

--- 409.js
+++ 409.js
@@ -3,29 +3,24 @@
  * @return {number}
  */
 const longestPalindrome = (s) => {
-  const map = new Map()
-  const len = s.length
+  const map = new Map();
+  const len = s.length;
   for (let i = 0; i < len; i++) {
     const curr = s[i];
     if (map.has(curr)) {
-      map.set(curr, map.get(curr) + 1)
+      map.set(curr, map.get(curr) + 1);
     } else {
-      map.set(curr, 1)
+      map.set(curr, 1);
     }
   }
-
-  let sum = 0
-  let more = false
+  let sum = 0;
   map.forEach((k, v) => {
-    if (k >= 2) {
-      const is_odd = k % 2 === 1
-      sum += is_odd ? k - 1 : k
-      if (is_odd) {
-        more = true
-      }
-      map.delete(v)
+    const is_odd = k % 2 === 1;
+    sum += (k >> 1) * 2;
+    if (is_odd && sum % 2 === 0) {
+      sum++
     }
-  })
+  });
 
-  return more || map.size > 0 ? sum + 1 : sum
+  return sum;
 };
