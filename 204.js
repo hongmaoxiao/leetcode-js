1d22b910c886da796489ff4d05d5beef42abf283
hongmaoxiao <769501294@qq.com>
2023-02-20 19:28:55 +0800

    add NO.204 another solution

--- 204.js
+++ 204.js
@@ -31,3 +31,18 @@ const isPrime = (n) => {
 
   return true
 }
+
+const countPrimes1 = (n) => {
+  const isPrime = new Array(n).fill(1)
+  let ans = 0
+  for (let i = 2; i < n; i++) {
+    if (isPrime[i]) {
+      ans += 1
+      for (let j = i * i; j < n; j += i) {
+        isPrime[j] = 0
+      }
+    }    
+  }
+
+  return ans
+}
