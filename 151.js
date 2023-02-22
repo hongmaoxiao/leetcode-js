52057ab887f0e1083d40a59a4d6b78589d910d7c
hongmaoxiao <769501294@qq.com>
2023-02-22 13:16:36 +0800

    add NO.151 another solution

--- 151.js
+++ 151.js
@@ -16,6 +16,36 @@ const reverseWords = (s) => {
   return arr.join(' ')
 };
 
-// console.log('sssss: ', reverseWords("the sky is blue"))
-// console.log('sssss: ', reverseWords("  hello world  "))
-console.log('sssss: ', reverseWords("a good   example"))
+const reverseWords1 = (s) => {
+  s = s.trim()
+  let res = []
+  const len = s.length
+  let left = 0, right = 0
+
+  while (right < len) {
+    let n = 0
+    while (s[right] !== ' ' && right < len) {
+      right++
+      n++
+      if (n === 10) {
+        break
+      }
+    }
+    res.push(s.slice(left, right))
+    while (s[right] === ' ') {
+      right++
+    }
+    left = right
+  }
+  reverse(res)
+  return res.join(' ')
+};
+
+const reverse = (arr) => {
+  let start = 0, end = arr.length - 1
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]]
+    start++
+    end--
+  }
+}
