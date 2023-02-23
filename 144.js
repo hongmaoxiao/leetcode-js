db1730cb266b323f6cff22a44ff837f192165c88
hongmaoxiao <769501294@qq.com>
2023-02-23 15:44:32 +0800

    add NO.144 another solution

--- 144.js
+++ 144.js
@@ -44,3 +44,33 @@ const preorderTraversal1 = (root) => {
     node = node.right
   }
 };
+
+const preorderTraversal2 = (root) => {
+  const res = []
+  if (!root) {
+    return res
+  }
+  let p1 = root, p2 = null
+
+  while (p1 !== null) {
+    p2 = p1.left
+    if (p2 !== null) {
+      while (p2.right !== null && p2.right !== p1) {
+        p2 = p2.right
+      }
+      if (p2.right === null) {
+        res.push(p1.val)
+        p2.right = p1
+        p1 = p1.left
+        continue
+      } else {
+        p2.right = null
+      }
+    } else {
+      res.push(p1.val)
+    }
+    p1 = p1.right
+  }
+
+  return res
+};
