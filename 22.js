766a6674d8d60067438ba9b1785e640b1ec22cdb
hongmaoxiao <769501294@qq.com>
2023-02-27 05:26:24 +0800

    add NO.22 another solution

--- 22.js
+++ 22.js
@@ -2,8 +2,29 @@
  * @param {number} n
  * @return {string[]}
  */
-// 暴力法
 const generateParenthesis = (n) => {
+  const arr = [];
+  backtrack(arr, '', 0, 0, n);
+  return arr;
+};
+
+const backtrack = (arr, str = '', open, close, len) => {
+  if (str.length === len * 2) {
+    arr.push(str)
+    return
+  }
+
+  if (open < len) {
+    backtrack(arr, `${str}(`, open + 1, close, len);
+  }
+
+  if (close < open) {
+    backtrack(arr, `${str})`, open, close + 1, len);
+  }
+}
+
+// 暴力法
+const generateParenthesis1 = (n) => {
   const arr = [];
   next(arr, n, 0);
   return arr;
