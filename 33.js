c7f6c2775575736d212da2ee4f4c79cfd2d1a520
hongmaoxiao <769501294@qq.com>
2023-02-27 20:04:38 +0800

    add NO.33 another solution

--- 33.js
+++ 33.js
@@ -40,3 +40,37 @@ const binary_search = (arr, target, start, end) => {
 
   return arr[start] === target ? start : -1
 }
+
+// 直接分半，一半一定有序，另一半可能有序也可能无序
+const search1 = (nums, target) => {
+  const len = nums.length
+  if (len === 0) {
+    return -1
+  }
+  if (len === 1) {
+    return nums[0] === target ? 0 : -1
+  }
+
+  let l = 0, r = len - 1
+  while (l <= r) {
+    const mid = (l + r) >> 1
+    if (nums[mid] === target) {
+      return mid
+    }
+
+    if (nums[0] <= nums[mid]) {
+      if (nums[0] <= target && target <= nums[mid]) {
+        r = mid - 1
+      } else {
+        l = mid + 1
+      }
+    } else {
+      if (nums[mid] <= target && target <= nums[len - 1]) {
+        l = mid + 1
+      } else {
+        r = mid - 1
+      }
+    }
+  }
+  return -1
+}
