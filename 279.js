b7e431c2dfc0a5f2753503e861aed1778b387ca7
hongmaoxiao <769501294@qq.com>
2023-03-01 23:19:33 +0800

    add NO.279 Perfect Squares

---
+++ 279.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const numSquares = (n) => {
+  const dp = [...Array(n + 1)].map(_ => 0)
+  for (let i = 1; i <= n; i++) {
+    dp[i] = i
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1)
+    }
+  }
+
+  return dp[n]
+};
