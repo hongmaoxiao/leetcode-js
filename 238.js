330be8e3d8f62f2aed020b5f170fa5b67511cdf8
hongmaoxiao <769501294@qq.com>
2023-03-01 10:12:47 +0800

    add NO.238 Product of Array Except Self

---
+++ 238.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const productExceptSelf = (nums) => {
+  const len = nums.length
+  let L = new Array(len).fill(1)
+  L[0] = 1
+  let R = new Array(len).fill(1)
+  R[len - 1] = 1
+  for (let i = 1; i < len; i++) {
+    L[i] = L[i - 1] * nums[i - 1]
+  }
+  for (let i = len - 2; i >= 0; i--) {
+    R[i] = R[i + 1] * nums[i + 1]
+  }
+
+  for (let i = 0; i < len; i++) {
+    nums[i] = L[i] * R[i] 
+  }
+
+  return nums
+};
