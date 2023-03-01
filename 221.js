c9e3343cedd97e22bab0a6e4c5c8acac179db9ad
hongmaoxiao <769501294@qq.com>
2023-03-01 21:49:10 +0800

    add NO.221 another solution

--- 221.js
+++ 221.js
@@ -39,3 +39,28 @@ const maximalSquare = (matrix) => {
 
   return maxSide * maxSide
 };
+
+const maximalSquare1 = (matrix) => {
+  let maxSide = 0;
+  if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
+    return maxSide;
+  }
+  const rows = matrix.length,
+    columns = matrix[0].length;
+  const dp = new Array(rows).fill(0).map(() => new Array(columns).fill(0))
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const curr = matrix[i][j];
+      if (curr === "1") {
+        if (i === 0 || j === 0) {
+          dp[i][j] = 1
+        } else {
+          dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+        }
+        maxSide = Math.max(maxSide, dp[i][j])
+      }
+    }
+  }
+
+  return maxSide * maxSide
+};
