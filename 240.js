e61f33f8eab53ef46d3a5f112a667c2e9b400d33
hongmaoxiao <769501294@qq.com>
2023-03-01 22:21:59 +0800

    add NO.240 another solution

--- 240.js
+++ 240.js
@@ -22,3 +22,42 @@ const searchMatrix = (matrix, target) => {
 
   return find
 };
+
+const searchMatrix1 = (matrix, target) => {
+  if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
+    return false
+  }
+
+  const rows = matrix.length, columns = matrix[0].length
+
+  for (let i = 0; i < rows; i++) {
+    if (matrix[i][0] > target) {
+      return false
+    }
+    if (matrix[i][columns - 1] < target) {
+      continue
+    }
+    const find = binary_search(matrix[i], target, 0, columns - 1)
+    if (find !== -1) {
+      return true
+    }
+  }
+
+  return false
+};
+
+const binary_search = (arr, target, start, end) => {
+  while (start < end) {
+    const mid = (start + end) >> 1
+    if (arr[mid] === target) {
+      return mid
+    }
+    if (arr[mid] < target) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+  }
+
+  return arr[start] === target ? start : -1
+}
