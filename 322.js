95e3abd263ddda5b3963708e815ca4cfd64913c7
hongmaoxiao <769501294@qq.com>
2023-03-02 16:06:53 +0800

    add NO.322 another solution

--- 322.js
+++ 322.js
@@ -3,6 +3,7 @@
  * @param {number} amount
  * @return {number}
  */
+// 自顶向下（递归）
 const coinChange = (coins, amount) => {
   if (amount === 0) {
     return 0
@@ -33,3 +34,22 @@ const coinChange = (coins, amount) => {
   dp(coins, amount)
   return count[count.length - 1]
 };
+
+// 动态规划 自底向上
+const coinChange1 = (coins, amount) => {
+  if (amount === 0) {
+    return 0
+  }
+
+  const dp = new Array(amount + 1).fill(amount + 1)
+  dp[0] = 0
+  for (let i = 1; i <= amount; i++) {
+    for (const coin of coins) {
+      if (i - coin < 0) {
+        continue
+      }
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1)
+    }
+  }
+  return dp[amount] === amount + 1 ? -1 : dp[amount]
+}
