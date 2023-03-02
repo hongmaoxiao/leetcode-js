4a14371d7773eef2a9df2cd03b4a775082f85873
hongmaoxiao <769501294@qq.com>
2023-03-02 12:42:03 +0800

    add NO.300 optimization

--- 309.js
+++ 309.js
@@ -20,3 +20,26 @@ const maxProfit = (prices) => {
 
   return Math.max(dp[dp.length - 1][1], dp[dp.length - 1][2])
 };
+
+const maxProfit1 = (prices) => {
+  const len = prices.length
+  if (len === 0) {
+    return 0
+  }
+
+  let f0 = -prices[0]
+  let f1 = 0
+  let f2 = 0
+
+  for (let i = 1; i < len; i++) {
+    const newf0 = Math.max(f0, f2 - prices[i])
+    const newf1 = f0 + prices[i]
+    const newf2 = Math.max(f1, f2)
+
+    f0 = newf0
+    f1 = newf1
+    f2 = newf2
+  }
+
+  return Math.max(f1, f2)
+};
