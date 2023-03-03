f02633fbad7267cda0f5ae7d67abb7a1f531ba7c
hongmaoxiao <769501294@qq.com>
2023-03-03 13:28:12 +0800

    add NO.416 another solution

--- 416.js
+++ 416.js
@@ -48,3 +48,36 @@ const canPartition = (nums) => {
 
   return dp[len - 1][target]
 };
+
+// 空间复杂度优化
+const canPartition1 = (nums) => {
+  const len = nums.length
+  if (len < 2) {
+    return false
+  }
+  let max = -Number.MAX_VALUE, sum = 0
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > max) {
+      max = nums[i]
+    } 
+    sum += nums[i] 
+  }
+
+  if (sum % 2 !== 0) {
+    return false
+  }
+
+  const target = sum / 2
+  if (max > target) {
+    return false
+  }
+
+  let dp = new Array(target + 1).fill(false)
+  dp[0] = true
+  for (let i = 0; i < len; i++) {
+    for (let j = target; j >= nums[i]; j--) {
+      dp[j] = dp[j] || dp[j - nums[i]]
+    }
+  }
+  return dp[target]
+};
