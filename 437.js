8e2dad244e7e2bd8b6b198108b89561b535abbc0
hongmaoxiao <769501294@qq.com>
2023-03-04 06:01:41 +0800

    add NO.437 another solution

--- 437.js
+++ 437.js
@@ -37,3 +37,26 @@ const rootSum = (node, targetSum) => {
 
   return ret
 }
+
+const pathSum1 = (root, targetSum) => {
+  const prefix = new Map()
+  prefix.set(0, 1)
+  return dfs(root, prefix, 0, targetSum)
+}
+
+const dfs = (node, prefix, curr, targetSum) => {
+  if (!node) {
+    return 0
+  }
+
+  let ret = 0
+  curr += node.val
+
+  ret = prefix.get(curr - targetSum) || 0
+  prefix.set(curr, (prefix.get(curr) || 0) + 1)
+  ret += dfs(node.left, prefix, curr, targetSum)
+  ret += dfs(node.right, prefix, curr, targetSum)
+  prefix.set(curr, (prefix.get(curr) || 0) - 1)
+
+  return ret
+}
