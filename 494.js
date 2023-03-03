6e3eff3df319a6b218d165d1a6b199b15fccc1f3
hongmaoxiao <769501294@qq.com>
2023-03-03 12:14:29 +0800

    add NO.494 another solution

--- 494.js
+++ 494.js
@@ -20,3 +20,39 @@ const findTargetSumWays = (nums, target) => {
   dfs(nums, 0, 0)
   return sum
 };
+
+const findTargetSumWays1 = (nums, target) => {
+  const len = nums.length
+  let sum = 0
+  for (let i = 0; i < len; i++) {
+    sum += nums[i]
+  }
+  if (Math.abs(target) > Math.abs(sum)) {
+    return 0
+  }
+  const large = 2 * sum + 1
+  let result = new Array(large).fill(0)
+  for (let i = 0; i < large; i++) {
+    if (i - sum === nums[0]) {
+      result[i] += 1
+    }
+    if (i - sum === -nums[0]) {
+      result[i] += 1
+    }   
+  }
+
+  for (let i = 1; i < len; i++) {
+    let temp = new Array(large).fill(0)
+    for (let j = 0; j < large; j++) {
+      const k = j - sum
+      if (k + nums[i] >= -sum && k + nums[i] <= sum) {
+        temp[j] += result[j + nums[i]]
+      }
+      if (k - nums[i] >= -sum && k - nums[i] <= sum) {
+        temp[j] += result[j - nums[i]]
+      }
+    }  
+    result = temp.slice()
+  }
+  return result[target + sum]
+};
