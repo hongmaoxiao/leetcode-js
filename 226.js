dfc453480071cdd18606616d5c7e47d5e13b55ce
hongmaoxiao <769501294@qq.com>
2023-03-05 17:38:53 +0800

    add NO.226 another solution

--- 226.js
+++ 226.js
@@ -10,6 +10,7 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
+// 深度优先搜索
 const invertTree = (root) => {
   if (!root) {
     return null
@@ -21,6 +22,32 @@ const invertTree = (root) => {
 
   invertTree(root.left)
   invertTree(root.right)
+
+  return root
+};
+
+// 广度优先搜索
+const invertTree1 = (root) => {
+  if (!root) {
+    return null
+  }
+
+  const queue = []
+  queue.unshift(root)
+  while (queue.length) {
+    const curr = queue.pop()
+    const temp = curr.left
+    curr.left = curr.right
+    curr.right = temp
+
+    if (curr.left) {
+      queue.unshift(curr.left)
+    }
+
+    if (curr.right) {
+      queue.unshift(curr.right)
+    }
+  }
 
   return root
 };
