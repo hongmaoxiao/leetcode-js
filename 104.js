6569a38d706ebbf7e294246d3358d6b71bd6c76c
hongmaoxiao <769501294@qq.com>
2023-03-05 13:53:03 +0800

    add NO.104 Maximum Depth of Binary Tree

---
+++ 104.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = (root) => {
+  let sum = 0
+
+  const backtrack = (node, curr) => {
+    if (!node) {
+      if (curr > sum) {
+        sum = curr
+      }
+      return 
+    }
+
+    curr += 1
+    backtrack(node.left, curr)
+    backtrack(node.right, curr)
+    curr -= 1
+  }
+
+  backtrack(root, 0)
+
+  return sum
+};
+
