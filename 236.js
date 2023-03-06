d4ae8467a755ddf5cf17356a53488d5d11f4d9a5
hongmaoxiao <769501294@qq.com>
2023-03-07 06:38:29 +0800

    add NO.236 Lowest Common Ancestor of a Binary Tree

---
+++ 236.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = (root, p, q) => {
+  let ans
+  const dfs = (root, p, q) => {
+    if (!root) {
+      return false
+    }
+    const lson = dfs(root.left, p, q)
+    const rson = dfs(root.right, p, q)
+    if ((lson && rson) || ((root.val === p.val || root.val === q.val) && (lson || rson))) {
+      ans = root
+    }
+    return lson || rson || (root.val === p.val || root.val === q.val)
+  }
+  dfs(root, p, q)
+  return ans
+};
