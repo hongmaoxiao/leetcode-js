49ce08b3f484365309ecc8994931d50e33570cc3
hongmaoxiao <769501294@qq.com>
2023-03-07 05:48:04 +0800

    add NO.114 Flatten Binary Tree to Linked List

---
+++ 114.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+const flatten = (root) => {
+  const ans = []
+  preorderTraversal(root, ans)
+  const len = ans.length
+  for (let i = 1; i < len; i++) {
+    const prev = ans[i - 1], curr = ans[i];
+    prev.left = null
+    prev.right = curr    
+  }
+};
+
+const preorderTraversal = (root, list) => {
+  if (root) {
+    list.push(root)
+    preorderTraversal(root.left, list)
+    preorderTraversal(root.right, list)
+  }
+}
+
+// 迭代+直接指向
+const flatten1 = (root) => {
+  if (!root) {
+    return
+  }
+  const stack = []
+  stack.push(root)
+  let prev = null
+  while (stack.length) {
+    const curr = stack.pop()
+    if (prev !== null) {
+      prev.left = null
+      prev.right = curr
+    }
+    if (curr.right) {
+      stack.push(curr.right)
+    }
+    if (curr.left) {
+      stack.push(curr.left)
+    }
+    prev = curr
+  }
+}
