1066a0aaab8ca1625d31ee6273026a10fc2a88fc
hongmaoxiao <769501294@qq.com>
2023-03-06 14:18:11 +0800

    add NO.112 another solution

--- 112.js
+++ 112.js
@@ -13,36 +13,73 @@
  */
 const hasPathSum = (root, targetSum) => {
   if (!root) {
-    return false
+    return false;
   }
-  let ans = false
+  let ans = false;
   const pathSum = (node, path) => {
     if (node) {
-      path += node.val
+      path += node.val;
 
       if (!node.left && !node.right) {
         if (path === targetSum) {
-          ans = true
+          ans = true;
         }
-        return
+        return;
       }
 
-      pathSum(node.left, path)
-      pathSum(node.right, path)
+      pathSum(node.left, path);
+      pathSum(node.right, path);
     }
-  }
-  pathSum(root, 0)
-  return ans
+  };
+  pathSum(root, 0);
+  return ans;
 };
 
 const hasPathSum1 = (root, targetSum) => {
   if (!root) {
-    return false
+    return false;
   }
 
   if (!root.left && !root.right) {
-    return root.val === targetSum
+    return root.val === targetSum;
   }
 
-  return hasPathSum1(root.left, targetSum - root.val) || hasPathSum1(root.right, targetSum - root.val)
-}
+  return (
+    hasPathSum1(root.left, targetSum - root.val) ||
+    hasPathSum1(root.right, targetSum - root.val)
+  );
+};
+
+const hasPathSum2 = (root, targetSum) => {
+  if (!root) {
+    return false;
+  }
+
+  const tree_queue = [];
+  const val_queue = [];
+  tree_queue.unshift(root);
+  val_queue.unshift(root.val);
+
+  while (tree_queue.length) {
+    const first_tree = tree_queue.pop();
+    const first_val = val_queue.pop();
+
+    if (!first_tree.left && !first_tree.right) {
+      if (first_val === targetSum) {
+        return true;
+      }
+    }
+
+    if (first_tree.left) {
+      tree_queue.unshift(first_tree.left);
+      val_queue.unshift(first_tree.left.val + first_val);
+    }
+
+    if (first_tree.right) {
+      tree_queue.unshift(first_tree.right);
+      val_queue.unshift(first_tree.right.val + first_val);
+    }
+  }
+
+  return false;
+};
